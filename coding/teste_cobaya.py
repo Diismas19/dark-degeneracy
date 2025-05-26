eeb892fc63e6167b00404e933d506ecb7ffcede6
Vitor Petri <diismas19@gmail.com>
2025-05-26 15:32:29 -0300

    update

--- coding/teste_cobaya.py
+++ coding/teste_cobaya.py
@@ -22,8 +22,8 @@ INT.set(cosmo)
 INT.set({'z_max_pk':2,'output':'tCl,lCl,mPk,mTk','k_output_values':0.05,'gauge':'newtonian','non linear':'hmcode','nonlinear_min_k_max':20,'N_ncdm': 1, 'N_ur': 2.0328})
 INT.compute()
 
-print(f'Diferença entre os H0: {100*INT.h() - dado_teste['H0']}')
-print(f'Diferença entre os Sigma8: {INT.sigma8() - dado_teste['sigma8']}')
+# print(f'Diferença entre os H0: {100*INT.h() - dado_teste['H0']}')
+# print(f'Diferença entre os Sigma8: {INT.sigma8() - dado_teste['sigma8']}')
 
 teste2 = '/home/vitor-petri/projects/cobaya/coding/vitor-cobaya-int/chains/int.1.txt'
 
@@ -37,16 +37,18 @@ dado_teste = {}
 for i in range(0,len(cabecalho)):
     dado_teste[cabecalho[i]] = float(first_line[i])
 
-keys = ['n_s','theta_s_100','omega_b','omega_cdm','w0_int','wa_int','tau_reio','A_s']
+keys = ['n_s','theta_s_100','omega_b','omega_cdm','w0_int','wa_int','tau_reio','A_s','omegamh2']
 
 cosmo = {k: dado_teste[k] for k in keys}
 cosmo['m_ncdm'] = 0.06
 
-INT = Class()
-INT.set(cosmo)
-INT.set({'z_max_pk':2,'output':'tCl,lCl,mPk,mTk','k_output_values':0.05,'gauge':'newtonian','non linear':'hmcode','nonlinear_min_k_max':20,'N_ncdm': 1, 'N_ur': 2.0328})
-INT.compute()
+print(cosmo['omegamh2'] - cosmo['omega_cdm'] - cosmo['omega_b'])
 
-print('Teste 2')
-print(f'Diferença entre os H0: {100*INT.h() - dado_teste['H0']}')
-print(f'Diferença entre os Sigma8: {INT.sigma8() - dado_teste['sigma8']}')
+# INT = Class()
+# INT.set(cosmo)
+# INT.set({'z_max_pk':2,'output':'tCl,lCl,mPk,mTk','k_output_values':0.05,'gauge':'newtonian','non linear':'hmcode','nonlinear_min_k_max':20,'N_ncdm': 1, 'N_ur': 2.0328})
+# INT.compute()
+
+# print('Teste 2')
+# print(f'Diferença entre os H0: {100*INT.h() - dado_teste['H0']}')
+# print(f'Diferença entre os Sigma8: {INT.sigma8() - dado_teste['sigma8']}')
